c8787dc1c89ee4df3c3b58702433ed4892064c15
Rath <rath@imls-dynip167.d.imb.unizh.ch>
2014-05-19 10:32:14 +0200

    shortlisting regions for class assignment

---
+++ Prisni/matrixloader.r
@@ -0,0 +1,31 @@
+c.sample <- read.table("~/Desktop/Prisni/Ovarian-Carcinoma-UID.txt")
+uid <- c()
+url <- c()
+for( i in 1:nrow(c.sample)){
+url[i] <- paste("http://www.progenetix.org/cgi-bin/api.cgi?db=arraymap&uid_m=", c.sample[[1]][i],"&output=matrix", sep="")
+}
+
+pg.matrix.loader <- function(url){
+	pg.frame <- read.table(url(url), header=T, sep="\t", na="NA")
+	pg.matrix <- as.matrix(pg.frame)
+	return(pg.matrix)
+} 
+
+write.to.file <- function(mat, gsm){
+write.table(mat, file=paste("~/Desktop/Prisni/OvaCa", gsm, sep="/"), quote=FALSE, row.names=T, col.names=T, sep="\t")
+print("written to file...")
+print(gsm)
+}
+
+
+pg.frame.final <- colnames(pg.matrix.loader(url[1]))
+for( i in 1:nrow(c.sample)){
+url <- paste("http://www.progenetix.org/cgi-bin/api.cgi?db=arraymap&uid_m=", c.sample[[1]][i],"&output=matrix", sep="")
+pg.frame.matrix <- pg.matrix.loader(url)
+pg.frame.final <- rbind(pg.frame.final, pg.frame.matrix)
+write.to.file(pg.frame.matrix, c.sample[[1]][i])
+}
+
+write.to.file(pg.frame.final, "gsm-all")
+
+pgframe1 <- read.table(url("http://www.progenetix.org/cgi-bin/api.cgi?db=arraymap&uid_m=GSM302781&output=matrix"), header=T, sep="\t", na="NA")
